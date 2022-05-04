f34b0ed9e9ebd7c9ac8b4e08bf21f7df6d41209c
pdavey1970 <104912232+pdavey1970@users.noreply.github.com>
2022-05-04 10:19:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+input.onSound(DetectedSound.Loud, function () {
+    sb.setServoPosition(sb.servo(SBServo.ServoA), 0)
+    basic.pause(2000)
+    sb.setServoPosition(sb.servo(SBServo.ServoA), 56)
+})
+input.setSoundThreshold(SoundThreshold.Loud, 128)
